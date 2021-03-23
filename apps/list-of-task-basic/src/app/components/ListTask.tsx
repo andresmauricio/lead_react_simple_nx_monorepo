0a262410dd89278ecca04c90d7ee7bd0825ddf13
andresmauricio <andresacelas@outlook.com>
2021-03-23 14:23:56 -0500

    send props component task

--- apps/list-of-task-basic/src/app/components/ListTask.tsx
+++ apps/list-of-task-basic/src/app/components/ListTask.tsx
@@ -3,11 +3,31 @@ import Task from './Task';
 import { Fragment } from 'react';
 
 export default function ListTask() {
+  const tasks = [
+    {
+      name: 'Install Visual Studio',
+      priority: 'Low',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+      status: true
+    },
+    {
+      name: 'Create Proyect',
+      priority: 'Medium',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+      status: false
+    },
+    {
+      name: 'Deploy Proyect',
+      priority: 'Higth',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+      status: true
+    }
+  ]
   return (
     <Fragment>
       {
-        [1, 2, 3].map((task) => <Task key={task} />)
+        tasks.map((task) => <Task props={task} key={task.name} />)
       }
     </Fragment>
   );
-}
+};
--- apps/list-of-task-basic/src/app/components/Task.tsx
+++ apps/list-of-task-basic/src/app/components/Task.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export default function Task() {
+export default function Task({props}) {
+  const { name, priority, description, status } = props;
+  console.log(props);
+  
   return (
     <article>
-      <header>Task Name</header>
-      <span>Priority</span>
-      <p>Description</p>
-      <p>Status</p>
+      <header>{name}</header>
+      <span>{priority}</span>
+      <p>{description}</p>
+      <p>{status}</p>
     </article>
-  )
+  );
 }
