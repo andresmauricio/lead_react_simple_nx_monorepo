2f6b5fbc7230c86cba62be0ac4f78472e013af97
andresmauricio <andresacelas@outlook.com>
2021-03-23 12:38:51 -0500

    create global styles for applications

--- apps/list-of-task-basic/src/app/app.tsx
+++ apps/list-of-task-basic/src/app/app.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
-  font-family: sans-serif;
-  min-width: 300px;
-  max-width: 600px;
-  margin: 50px auto;
+
 `;
 
 export function App() {
--- apps/list-of-task-basic/src/main.tsx
+++ apps/list-of-task-basic/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './app/app';
 
+import GlobalStyle from './global/Global';
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
