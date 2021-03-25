8c7ff69ef268934948dfc558adfff2dc669567e1
andresmauricio <andresacelas@outlook.com>
2021-03-25 16:43:51 -0500

    save data in state

--- apps/open-library-basic-api/src/app/app.tsx
+++ apps/open-library-basic-api/src/app/app.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from './components/Table';
 import GlobalStyle from './GlobalStyle';
 
 const API = 'https://openlibrary.org/recentchanges.json?limit=30';
 
 export function App() {
+  const [openLibrary, setOpenLibrary] = useState([]);
   const getData = async () => {
     const response = await fetch(API);
     const data = await response.json();
-    console.log(data);
+    setOpenLibrary(data);
   };
 
   useEffect(() => {
