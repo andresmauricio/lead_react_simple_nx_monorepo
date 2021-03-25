03f946f24c15779bcbc45a0737c930807ef76138
andresmauricio <andresacelas@outlook.com>
2021-03-25 16:58:46 -0500

    styles of table

--- apps/open-library-basic-api/src/app/components/Table.tsx
+++ apps/open-library-basic-api/src/app/components/Table.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Table({ books }) {
   return (
-    <table className="table">
+    <table className="table is-fullwidth is-hoverable">
       <thead>
         <tr>
-          <th>Id</th>
-          <th>Kind</th>
-          <th>Time</th>
-          <th>Comment</th>
-          <th>Ip</th>
-          <th>Number Changes</th>
+          <th className="title-5 has-text-info">Id</th>
+          <th className="title-5 has-text-info">Kind</th>
+          <th className="title-5 has-text-info">Time</th>
+          <th className="title-5 has-text-info">Comment</th>
+          <th className="title-5 has-text-info">Ip</th>
+          <th className="title-5 has-text-info">Number Changes</th>
         </tr>
       </thead>
       <tbody>
@@ -20,7 +20,7 @@ function Table({ books }) {
             <th>{book.kind}</th>
             <th>{book.timestamp}</th>
             <th>{book.comment}</th>
-            <th>{book.ip}</th>
+            <th>{book.ip || new Date().getTime()}</th>
             <th>{book.changes.length}</th>
           </tr>
         ))}
