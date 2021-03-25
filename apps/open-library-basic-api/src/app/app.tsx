0182fa4fa2caabdce34cf79e6a49572cf30bca50
andresmauricio <andresacelas@outlook.com>
2021-03-25 16:36:56 -0500

    get data api

--- apps/open-library-basic-api/src/app/app.tsx
+++ apps/open-library-basic-api/src/app/app.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Table from './components/Table';
 import GlobalStyle from './GlobalStyle';
 
+const API = 'https://openlibrary.org/recentchanges.json?limit=30';
+
 export function App() {
+  const getData = async () => {
+    const response = await fetch(API);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
