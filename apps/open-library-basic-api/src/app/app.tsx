18d60a00aa80bf6c8ac5a9e5437b5af9c086765b
andresmauricio <andresacelas@outlook.com>
2021-03-25 16:49:04 -0500

    render info api

--- apps/open-library-basic-api/src/app/app.tsx
+++ apps/open-library-basic-api/src/app/app.tsx
@@ -19,7 +19,7 @@ export function App() {
   return (
     <>
       <GlobalStyle />
-      <Table />
+      <Table books={openLibrary} />
     </>
   );
 }
--- apps/open-library-basic-api/src/app/components/Table.tsx
+++ apps/open-library-basic-api/src/app/components/Table.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 
-function Table() {
+function Table({ books }) {
   return (
     <table>
-      <tr>
-        <th>Id</th>
-        <th>Kind</th>
-        <th>Time</th>
-        <th>Comment</th>
-        <th>Ip</th>
-        <th>Number Changes</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Kind</th>
+          <th>Time</th>
+          <th>Comment</th>
+          <th>Ip</th>
+          <th>Number Changes</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map((book) => (
+          <tr key={book.id}>
+            <th>{book.id}</th>
+            <th>{book.kind}</th>
+            <th>{book.timestamp}</th>
+            <th>{book.comment}</th>
+            <th>{book.ip}</th>
+            <th>{book.changes.length}</th>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
