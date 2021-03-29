a3b04b2c6b4a75a932f06c3bedbea8a38c859947
andresmauricio <andresacelas@outlook.com>
2021-03-28 20:36:04 -0500

    style herader

--- apps/badges/src/app/components/Navbar.tsx
+++ apps/badges/src/app/components/Navbar.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Navbar() {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
+  return <Header>Hello</Header>;
 }
 
-export default Navbar
+export default Navbar;
+
+const Header = styled.header`
+  width: 100%;
+  height: 60px;
+  background-color: #1c3643;
+  display: flex;
+  align-items: center;
+  padding: 2px;
+  color: white;
+`;
