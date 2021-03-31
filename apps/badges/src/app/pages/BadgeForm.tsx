40aac6ac1ca6ebc652c3159eb1cd846259238093
andresmauricio <andresacelas@outlook.com>
2021-03-31 18:57:44 -0500

    create form for add badge

--- apps/badges/src/app/pages/BadgeForm.tsx
+++ apps/badges/src/app/pages/BadgeForm.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 function BadgeForm() {
   return (
     <div>
-      add badge
-      <h1>nes</h1>
+      <Navbar />
+      <form>
+        <div className="form-group">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Jhon Doe"
+            id="fullName"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="twitter">Twitter</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="@doejoe"
+            id="twitter"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="profesionIn">Profesion</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Chif Officer Manager"
+            id="profesionIn"
+          />
+        </div>
+        <button type="submit" className="btn btn-success btn-block">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
