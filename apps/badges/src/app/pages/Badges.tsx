e62f1a6550e34d4678cbcbb4371e3c56a64f531f
andresmauricio <andresacelas@outlook.com>
2021-03-30 20:30:44 -0500

    create banner section in page badges

--- apps/badges/src/app/pages/Badges.tsx
+++ apps/badges/src/app/pages/Badges.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Austronauta from '../../assets/astronauta.png';
+import styled from 'styled-components';
 
 function Badges() {
   return (
-    <div>
-      Hello Baby! 
-    </div>
-  )
+    <>
+      <Navbar />
+      <Section>
+        <Img src={Austronauta} alt="" />
+      </Section>
+    </>
+  );
 }
 
-export default Badges
+export default Badges;
+
+const Section = styled.section`
+  width: 100%;
+  height: 250px;
+  background-color: #1b1b25;
+  background-image: url('../../assets/stars.svg');
+  background-repeat: repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Img = styled.img`
+  height: 180px;
+`;
