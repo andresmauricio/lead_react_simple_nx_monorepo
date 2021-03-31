bd0a1b523808cc3827f3bf94a2a4386db7a190bb
andresmauricio <andresacelas@outlook.com>
2021-03-31 16:45:06 -0500

    card badges styles

--- apps/badges/src/app/pages/Badges.tsx
+++ apps/badges/src/app/pages/Badges.tsx
@@ -10,6 +10,30 @@ function Badges() {
       <Section>
         <Img src={Austronauta} alt="" />
       </Section>
+      <ContainerBadges>
+        <Article>
+          <ImgProfile
+            src="https://images.unsplash.com/photo-1505999407077-7937810b98ae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=888&q=80"
+            alt="Image profile"
+          />
+          <div>
+            <h3>Paola Vargas</h3>
+            <span>@pvargas</span>
+            <p>Contador publico</p>
+          </div>
+        </Article>
+        <Article>
+          <ImgProfile
+            src="https://images.unsplash.com/photo-1505999407077-7937810b98ae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=888&q=80"
+            alt="Image profile"
+          />
+          <div>
+            <h3>Paola Vargas</h3>
+            <span>@pvargas</span>
+            <p>Contador publico</p>
+          </div>
+        </Article>
+      </ContainerBadges>
     </>
   );
 }
@@ -29,4 +53,26 @@ const Section = styled.section`
 
 const Img = styled.img`
   height: 180px;
+`;
+
+const Article = styled.article`
+  width: 550px;
+  height: 150px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  margin: 20px auto;
+  padding: 0 20px;
+`;
+
+const ImgProfile = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+const ContainerBadges = styled.section`
+  width: 100%;
+  background-color: #eaeaec;
 `;
