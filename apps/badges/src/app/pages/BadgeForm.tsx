7e44728a51503223bec5165b7041f1277886d273
andresmauricio <andresacelas@outlook.com>
2021-03-31 19:33:48 -0500

    create compoent form add

--- apps/badges/src/app/pages/BadgeForm.tsx
+++ apps/badges/src/app/pages/BadgeForm.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 
 function BadgeForm() {
+  const person = {
+    name: '',
+    profesion: '',
+    twitter: '',
+  };
+  const [personForm, setPerson] = useState(person);
+  const handlerChange = (e) => {
+    const { value, name } = e.target;
+    setPerson({ ...personForm, [name]: value });
+  };
   return (
     <div>
       <Navbar />
       <Container>
+        <section>
+          <p>Name: {personForm.name}</p>
+          <p>Profesion: {personForm.profesion}</p>
+          <p>Twitter: {personForm.twitter}</p>
+        </section>
         <form>
           <div className="form-group">
             <label htmlFor="fullName">Full Name</label>
@@ -15,15 +30,21 @@ function BadgeForm() {
               className="form-control"
               placeholder="Jhon Doe"
               id="fullName"
+              name="name"
+              value={personForm.name}
+              onChange={(e) => handlerChange(e)}
             />
           </div>
           <div className="form-group">
             <label htmlFor="twitter">Twitter</label>
             <input
-              type="email"
+              type="text"
               className="form-control"
               placeholder="@doejoe"
               id="twitter"
+              name="twitter"
+              value={personForm.twitter}
+              onChange={(e) => handlerChange(e)}
             />
           </div>
           <div className="form-group">
@@ -33,6 +54,9 @@ function BadgeForm() {
               className="form-control"
               placeholder="Chif Officer Manager"
               id="profesionIn"
+              name="profesion"
+              value={personForm.profesion}
+              onChange={(e) => handlerChange(e)}
             />
           </div>
           <button type="submit" className="btn btn-success btn-block">
